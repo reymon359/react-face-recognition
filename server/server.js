4420f5a28c80860957c624ea2f88e489135ff3e9
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-05 11:35:43 +0200

    feat(server & client): update server port connection & add Environmental vars

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -10,27 +11,37 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+	},
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+	res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+	register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+	image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+	image.handleApiCall(req, res);
+});
 
-app.listen(3001, ()=> {
-  console.log('app is running on port 3001');
-})
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
+});
