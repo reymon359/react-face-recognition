34fb95fa2f3d7a7180808d2d17458445fa5e873d
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-05 19:40:13 +0200

    feat(server): add Postgres Database and connect API to it

--- server/server.js
+++ server/server.js
@@ -13,12 +13,7 @@ const morgan = require('morgan');
 
 const db = knex({
 	client: 'pg',
-	connection: {
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
-		database: process.env.DB_NAME,
-	},
+	connection: process.env.POSTGRES_URI
 });
 
 const app = express();
