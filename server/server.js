4f3e2b302bf19bdf430f332faceac47b4832d7a3
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-18 10:58:14 +0200

    feat(server): add Redis to the server &  Docker Compose

--- server/server.js
+++ server/server.js
@@ -9,8 +9,10 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 const morgan = require('morgan');
 
+//Database Setup
 const db = knex({
 	client: 'pg',
 	connection: process.env.POSTGRES_URI,
@@ -22,23 +24,20 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.send(db.users);
-});
 app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => {
 	register.handleRegister(req, res, db, bcrypt);
 });
-app.get('/profile/:id', (req, res) => {
+app.get('/profile/:id', auth.requireAuth, (req, res) => {
 	profile.handleProfileGet(req, res, db);
 });
-app.post('/profile/:id', (req, res) => {
+app.post('/profile/:id', auth.requireAuth, (req, res) => {
 	profile.handleProfileUpdate(req, res, db);
 });
-app.put('/image', (req, res) => {
+app.put('/image', auth.requireAuth, (req, res) => {
 	image.handleImage(req, res, db);
 });
-app.post('/imageurl', (req, res) => {
+app.post('/imageurl', auth.requireAuth, (req, res) => {
 	image.handleApiCall(req, res);
 });
 
